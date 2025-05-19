7c592dfbf5cf1a95f0956dc768c7946eeb5f5408
Annie Wu <annie25506@gmail.com>
2025-05-20 00:51:26 +0800

    feat: vireptess img zoom

--- docs/.vitepress/theme/index.js
+++ docs/.vitepress/theme/index.js
@@ -1,4 +1,14 @@
 import DefaultTheme from 'vitepress/theme'
 import './custom.css'
 
-export default DefaultTheme
+export default {
+  ...DefaultTheme,
+  setup() {
+    document.addEventListener('click', (e) => {
+      const target = e.target;
+      if (target.tagName === 'IMG' && target.closest('.VPDoc')) {
+        target.classList.toggle('zoomed');
+      }
+    });
+  }
+}
