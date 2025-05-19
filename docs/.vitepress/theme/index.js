bb5b397c6c13389f77255bc9e8f93869500e955b
Annie Wu <annie25506@gmail.com>
2025-05-20 00:57:57 +0800

    fix: note image zoom out

--- docs/.vitepress/theme/index.js
+++ docs/.vitepress/theme/index.js
@@ -6,8 +6,19 @@ export default {
   setup() {
     document.addEventListener('click', (e) => {
       const target = e.target;
+      const zoomedImg = document.querySelector('.VPDoc img.zoomed') ;
+
       if (target.tagName === 'IMG' && target.closest('.VPDoc')) {
-        target.classList.toggle('zoomed');
+        if (target.classList.contains('zoomed')) {
+          target.classList.remove('zoomed'); 
+        } else {
+          if (zoomedImg && zoomedImg !== target) {
+            zoomedImg.classList.remove('zoomed');
+          }
+          target.classList.add('zoomed');
+        }
+      } else if (zoomedImg) {
+        zoomedImg.classList.remove('zoomed');
       }
     });
   }
